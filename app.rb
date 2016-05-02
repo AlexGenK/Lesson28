cfb5112ac00ecfb9a1ec846aa80ce7e7b805ee46
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-05-02 18:04:29 +0300

    add database

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 # блог
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 # процедура инициализации
 configure do
-
+	# подключаемся, а при отсутствии создаем БД
+	$db=SQLite3::Database.new "./database.db"
+	# результаты запроса выводятся в виде хеша
+	$db.results_as_hash = true
+	$db.execute("CREATE  TABLE  IF NOT EXISTS posts (p_id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, 
+													 p_name VARCHAR, 
+													 p_post VARCHAR, 
+													 p_date DATETIME DEFAULT (datetime('now', 'localtime')))")
 end
 
 # главная страница
@@ -23,6 +31,7 @@ post '/new' do
   @post=params[:post]
   @error=get_error_message({:name=>"Enter your name. ", :post=>"Enter your post"})
   if @error==""
+  	$db.execute("INSERT INTO posts (p_name, p_post) VALUES (?, ?)", [@name, @post])
   	erb "<p>#{@name} add post: #{@post}</p>"
   else
   	erb :new
